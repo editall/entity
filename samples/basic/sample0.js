fac93d6874cdb059b2ceeb2fadcc2b97822ee99b
hika <hika@bsidesoft.com>
2022-01-05 17:05:13 +0900

    add basic samples

---
+++ samples/basic/sample0.js
@@ -0,0 +1,29 @@
+import {Entity} from "../../src/entity/Entity.js";
+import {stringValue} from "../../src/field/string/StringField.js";
+import {numberValue} from "../../src/field/number/NumberField.js";
+import {entityList} from "../../src/field/entity/EntityListField.js";
+
+class Partner extends Entity{
+    constructor() {
+        super();
+        stringValue(this, "name");
+    }
+}
+class Member extends Entity{
+    constructor() {
+        super();
+        stringValue(this, "name");
+        numberValue(this, "age");
+        entityList(this, "partners", Partner);
+    }
+}
+
+const member = (new Member).parse({
+    name:"hika",
+    age:18,
+    partners:[
+        {name:"jidolstar"},
+        {name:"easy"}
+    ]
+})
+console.log(member);
