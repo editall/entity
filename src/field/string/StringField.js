45cec3ebbb23c712087dc9a2aa87cff94ab736c7
hika <hika@bsidesoft.com>
2022-01-24 16:13:08 +0900

    remove replace

--- src/field/string/StringField.js
+++ src/field/string/StringField.js
@@ -2,18 +2,6 @@ import {Field} from "../Field.js";
 
 class StringField extends Field{
     typeValidation(v){ return typeof v == "string";}
-    toJSON(){
-        return this.v
-            .replaceAll("\"", "\\\"")
-            .replaceAll("\n", "\\n")
-            .replaceAll("\r", "\\\\r");
-    }
-    fromJSON(v){
-        return v
-            .replaceAll("\\\"", "\"")
-            .replaceAll("\\n", "\n")
-            .replaceAll("\\r", "\r");
-    }
 }
 const stringValue = ()=>new StringField;
 export {stringValue};
